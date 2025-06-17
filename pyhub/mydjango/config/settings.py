73adf4228ae6cc6c00f3059277882fa2a68af6a6
Chinseok Lee <me@askcompany.kr>
2025-06-17 12:03:14 +0900

    chat 앱 생성 및 등록

--- pyhub/mydjango/config/settings.py
+++ pyhub/mydjango/config/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # new app
+    'chat',
 ]
 
 MIDDLEWARE = [
@@ -77,7 +79,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
-}
+}       
 
 
 # Password validation
