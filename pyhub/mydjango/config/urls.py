83b5b3af7ac0e529b9f4fe16674a2c31506838c8
Chinseok Lee <me@pyhub.kr>
2025-06-17 16:00:46 +0900

    js를 통한 채팅 메시지 전송 구현 중

--- pyhub/mydjango/config/urls.py
+++ pyhub/mydjango/config/urls.py
@@ -3,5 +3,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # chat/urls에 있는 모든 URL 패턴에 일괄적으로
+    #  chat/ 라는 prefix 주소를 부여하겠다.
     path('chat/', include('chat.urls')),
 ]
