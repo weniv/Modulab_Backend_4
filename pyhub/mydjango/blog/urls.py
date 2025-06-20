3d7a26239a816e6fb911181eddaa7ac305689801
Chinseok Lee <me@pyhub.kr>
2025-06-20 15:59:59 +0900

    blog.Post 모델에 대한 list/detail 뷰 간단 구현

--- pyhub/mydjango/blog/urls.py
+++ pyhub/mydjango/blog/urls.py
@@ -5,4 +5,7 @@ from . import views
 
 # 장고의 urls.py에게 장고가 요구하는 것은 단 하나 !!
 # urlpatterns 이름의 리스트 => 이름에 오타가 있으면 안 되요.
-urlpatterns = []
+urlpatterns = [
+    path("", views.post_list),
+    path("<int:pk>/", views.post_detail),
+]
