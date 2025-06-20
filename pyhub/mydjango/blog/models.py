9ea7d4936adee4f5d45be7549b6108704f3f9287
Chinseok Lee <me@pyhub.kr>
2025-06-20 16:43:01 +0900

    blog.Comment 모델 정의 및 마이그레이션 파일 생성

--- pyhub/mydjango/blog/models.py
+++ pyhub/mydjango/blog/models.py
@@ -26,3 +26,23 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+
+# 모델에 기본 키를 지정해서 기본키를 변경하는 방법이 있습니다.
+# 이를 지정하지 않으면, id 필드가 자동 생성됩니다.
+
+class Comment(models.Model):
+    # 댓글 길이 제한을 두지 않으려면.
+    # content = models.TextField()
+
+    # Post의 기본키를 가리키는 필드 => 외래키 (Foreign Key)
+    post = models.ForeignKey(
+        Post,
+        # 바라보고 있는 Post이 삭제되었을 때, 관련된 댓글은
+        # 어떤 처리를 해야할까?
+        on_delete=models.CASCADE,  # 관련 댓글 자동 삭제
+        # 이 외에도 Post 삭제 막기, etc.
+    )
+    content = models.CharField(
+        max_length=1000,
+    )
