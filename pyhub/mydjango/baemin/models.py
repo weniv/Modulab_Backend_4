1fe21d1c66d88d16ed3daf63ccaaa58dbbcd3c2b
Chinseok Lee <me@pyhub.kr>
2025-06-25 14:42:30 +0900

    custom validator 구현

--- pyhub/mydjango/baemin/models.py
+++ pyhub/mydjango/baemin/models.py
@@ -1,6 +1,6 @@
 # baemin/models.py
-
-
+from django.core.exceptions import ValidationError
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
 
 
@@ -12,13 +12,46 @@ class Shop(models.Model):
     photo = models.ImageField()  # 이미지만 받아요.
 
 
+# 장고의 유효성 검사 함수는 인자는 항상 1개만 받구요.
+# 그 값이 정해진 규칙에서 벗어날 때, ValidationError 예외를 발생.
+# 정해진 규칙에 부합이 될 때, Nothing to do. 그냥 함수 종료. 반환값 필요 X.
+# def validator_min_1(value):
+#     """인자의 값이 1 이상이기를 기대합니다."""
+#     if value < 1:
+#         raise ValidationError("값이 1이상이어야 합니다.")
+#
+#
+# def validator_min_3(value):
+#     """인자의 값이 3 이상이기를 기대합니다."""
+#     if value < 3:
+#         raise ValidationError("값이 3이상이어야 합니다.")
+
+# 위 함수를 만들어주는 함수를 만들어보자 !!! <= JS에서도 활발히 사용되는 패턴.
+def make_validator_min(min_value):
+    def validator_func(value):
+        if value < min_value:
+            raise ValidationError(f"값이 {min_value}이상 이어야 합니다.")
+
+    return validator_func
+
+# validator_min_3 = make_validator_min(3)
+
+
 # N 측에 1에 대한 외래키 필드를 추가
 class Review(models.Model):
     shop = models.ForeignKey(Shop, on_delete=models.CASCADE)
     content = models.TextField()
     # rating = models.IntegerField()  # 음수/양수 다 담을 수 있어요.
     # rating = models.PositiveIntegerField()   # 양수만 담을 수 있어요. 담을 수 있는 양수의 범위가 2배 커져요.
-    rating = models.PositiveSmallIntegerField()  #
+    rating = models.PositiveSmallIntegerField(
+        # Validator : 입력된 값에 대한 유효성 검사를 수행해주는 다수의 함수
+        validators=[
+            MinValueValidator(1),    # 가급적 장고 기본에서 지원하는 Validators를 최대한 활용해주세요.
+            # make_validator_min(1),  # 함수가 함수를 생성한 버전
+            MaxValueValidator(5),   # 클래스를 함수처럼 사용한 버전
+        ],
+        help_text="1점 ~ 5점 사이 점수를 주세요."
+    )  #
 
     # 1 bit => 0 과 1을 표현 => 값을 2가지 표현 가능
     # 2 bit => 2 ** 2 => 총 4가지 표현 가능
