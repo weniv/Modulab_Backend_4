ad0d7dcea88d48a87ab67724a828dbe38a057aca
Chinseok Lee <me@pyhub.kr>
2025-06-26 10:33:08 +0900

    최상위 주소 요청에 대해 /baemin/ 주소로 이동 응답

--- pyhub/mydjango/config/urls.py
+++ pyhub/mydjango/config/urls.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
+from django.shortcuts import redirect
 from django.urls import path, include
 from debug_toolbar.toolbar import debug_toolbar_urls
 
@@ -11,6 +12,10 @@ urlpatterns = [
     path('chat/', include('chat.urls')),
     path('blog/', include('blog.urls')),  # 별도의 tuple
     path('baemin/', include('baemin.urls')),
+    # 어떤 응답을 할까?
+    #  - 1) Template을 통해 HTML 응답을 할까?
+    #  - 2) 다른 페이지로 이동을 시켜볼 수도 있겠습니다.
+    path('', lambda request: redirect("/baemin/")),
 ]
 
 # 유저가 업로드한 파일을 서빙하는 View 설정 (View <- 장고 개발서버에서 제공)
