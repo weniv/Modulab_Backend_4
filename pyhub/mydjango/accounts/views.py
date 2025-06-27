85ecefd7b65b72cd38df13401e4a81c0ed9fc103
Chinseok Lee <me@pyhub.kr>
2025-06-27 16:32:55 +0900

    SignupForm을 통한 username 회원가입 시에 유효성 검사 룰 추가

--- pyhub/mydjango/accounts/views.py
+++ pyhub/mydjango/accounts/views.py
@@ -1,13 +1,13 @@
 # accounts/views.py
 
-from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import redirect, render
+from .forms import SignupForm
 
 def signup(request):
     if request.method == "GET":
-        form = UserCreationForm()
+        form = SignupForm()
     else:
-        form = UserCreationForm(data=request.POST, files=request.FILES)
+        form = SignupForm(data=request.POST, files=request.FILES)
         if form.is_valid():
             form.save()  # 회원가입 완료 !! 지정 유저 레코드 DB에 저장됨 !!!
             # 회원가입을 했으니, 로그인 페이지로 이동하는 것이 자연스러워요.
