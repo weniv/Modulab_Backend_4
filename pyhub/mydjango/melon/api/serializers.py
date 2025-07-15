71ca903a5da650329498c3109f80ed3c8fb9f216
Chinseok Lee <me@pyhub.kr>
2025-07-15 11:25:07 +0900

    melon.Song song_list api 응답에서 title_length 속성 추가

--- pyhub/mydjango/melon/api/serializers.py
+++ pyhub/mydjango/melon/api/serializers.py
@@ -6,6 +6,8 @@ from melon.models import Song
 
 
 # class SongForm(forms.ModelForm):
+#     comment = forms.CharField()
+#
 #     class Meta:
 #         model = Song
 #         fields = "__all__"
@@ -15,6 +17,14 @@ from melon.models import Song
 
 # 변환, 요청 처리
 class SongSerializer(serializers.ModelSerializer):
+    # 모델 필드에는 없는 이름이어야 합니다. 존재하는 이름이면 덮어쓰기가 됩니다.
+    title_length = serializers.SerializerMethodField()
+
+    def get_title_length(self, song) -> int:
+        return len(song.title)
+
     class Meta:
         model = Song
-        fields = "__all__"
+        # fields = "__all__"  # 추천드리지 않아요. 의도치않게 모델 필드가 api로 노출될 수 있습니다.
+        # 명시적인 지정을 추천드립니다.
+        fields = ["id", "rank", "album", "title", "artist", "title_length"]
--- pyhub/mydjango/melon/api/v1.py
+++ pyhub/mydjango/melon/api/v1.py
@@ -4,12 +4,17 @@ from django.urls import path
 from melon.models import Song
 from melon.api.serializers import SongSerializer
 
+# View 단에서는 데이터가 어떻게 변환되는 지에 대해서 관심을 두지 않습니다.
+#  - 관심사의 분리
+
 def song_list(request):
     qs = Song.objects.all()
 
+    # 데이터 변환의 기능을 제공 (시리얼라이저 오리지널 기능)
+    #  - 입력 데이터에 대한 유효성 검사 및 저장 (ModelForm의 기능)
     serializer = SongSerializer(
-        instance=qs,
-        many=True,
+        instance=qs,  # Model Instance 또는 QuerySet
+        many=True,  # Model Instance 시에는 False, QuerySet 시에는 True
     )
 
     # JSON 문자열로 변환 (Serialize)해서, 해당 문자열을 반환
@@ -20,13 +25,16 @@ def song_list(request):
     #         "title": song.title,
     #         "artist": song.artist,
     #         # 이 계산을 누가 할래 ??? (BE, FE, 혹은 어느 누군가?)
-    #         "title_length": len(song.title),  # 계산된 값.
+    #         "title_length": len(song.title),  # 계산된 속성
     #     }
     #     for song in qs
     # ]
-    song_list_data = serializer.data  # 자동 변환
+    song_list_data = serializer.data  # 자동 변환을 즉시 수행.
 
+    # DRF, Renderer
     json_string = json.dumps(song_list_data, ensure_ascii=False)
+
+    # 응답
     return HttpResponse(json_string, content_type="application/json")
 
 
