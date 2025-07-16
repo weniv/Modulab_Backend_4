df94a8e109278f91d3c9c1f22fd658447c0e695a
Chinseok Lee <me@pyhub.kr>
2025-07-16 16:34:47 +0900

    pagination type, page_size에 따른 클래스 생성 함수 구현

--- pyhub/mydjango/blog/pagination.py
+++ pyhub/mydjango/blog/pagination.py
@@ -1,3 +1,6 @@
+import re
+from typing import Type, cast, Literal
+from rest_framework import pagination as drf_pagination
 from rest_framework.pagination import CursorPagination
 
 
@@ -5,3 +8,30 @@ class PkCursorPagination(CursorPagination):
     # 커서 기반 페이징을 위해 정렬 순서 지정
     #  - 고유성, 불변성, 인덱싱
     ordering = "-id"
+
+
+def make_pagination_class(
+    cls_type: Literal["page_number", "limit_offset", "cursor"],
+    page_size: int,
+    max_limit: int = None,
+    cursor_ordering: str = None,
+) -> Type[drf_pagination.BasePagination]:
+    base_cls_name = f"{cls_type.title().replace('_', '')}Pagination"
+    base_cls = getattr(drf_pagination, base_cls_name)
+
+    if cls_type == "page_number":
+        cls_name = f"{base_cls_name}WithPageSize{page_size}"
+        attrs = {"page_size": page_size}
+    elif cls_type == "limit_offset":
+        cls_name = f"{base_cls_name}WithDefaultLimit{page_size}AndMaxLimit{max_limit}"
+        attrs = {"default_limit": page_size, "max_limit": max_limit}
+    elif cls_type == "cursor":
+        ordering = (cursor_ordering or "").title().replace("_", "")
+        ordering = re.sub(r"^[+-]", "", ordering)
+        cls_name = f"{base_cls_name}WithPageSize{page_size}AndOrdering{ordering}"
+        attrs = {"page_size": page_size, "ordering": cursor_ordering}
+    else:
+        raise NotImplementedError(f"Not implemented cls_type : {cls_type}")
+
+    cls = type(cls_name, (base_cls,), attrs)
+    return cast(Type[drf_pagination.BasePagination], cls)
