30e2cc1f917da07ca8e59f1388f0c2e0f472360c
Chinseok Lee <me@pyhub.kr>
2025-07-16 10:37:12 +0900

    Post Detail API를 구현하고, List 응답에서는 content 필드를 제외

--- pyhub/mydjango/blog/api/serializers.py
+++ pyhub/mydjango/blog/api/serializers.py
@@ -16,3 +16,16 @@ class PostSerializer(serializers.ModelSerializer):
             "created_at",
             "updated_at",
         ]
+
+
+class PostListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Post
+        fields = [
+            "id",
+            "title",
+            # "content",  # content 필드는 노출하지 않습니다.
+            "status",
+            "created_at",
+            "updated_at",
+        ]
--- pyhub/mydjango/blog/api/v1.py
+++ pyhub/mydjango/blog/api/v1.py
@@ -1,18 +1,25 @@
 # blog/api/v1.py
 
 from django.urls import path
-from rest_framework.generics import ListAPIView
+from rest_framework.generics import ListAPIView, RetrieveAPIView
 from blog.models import Post
-from blog.api.serializers import PostSerializer
+from blog.api.serializers import PostSerializer, PostListSerializer
 
 
 post_list = ListAPIView.as_view(
     # queryset=Post.objects.filter(status=Post.Status.PUBLISHED),
     queryset=Post.objects.published(),
-    serializer_class=PostSerializer,  # 고정. but 동적 지정도 가능.
+    serializer_class=PostListSerializer,  # 고정. but 동적 지정도 가능.
+)
+
+
+post_detail = RetrieveAPIView.as_view(
+    queryset=Post.objects.published(),
+    serializer_class=PostSerializer,
 )
 
 
 urlpatterns = [
     path("posts/", post_list),
+    path("posts/<int:pk>/", post_detail),
 ]
