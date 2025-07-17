f270f4a0f8b8358aa934af6e6ec08f3f3d8ef47a
Chinseok Lee <me@pyhub.kr>
2025-07-17 11:43:40 +0900

    pytest-django에서 client 객체를 fixture를 통해 주입

--- pyhub/mydjango/blog/tests/test_blog_posts.py
+++ pyhub/mydjango/blog/tests/test_blog_posts.py
@@ -1,11 +1,9 @@
 import pytest
-from django.test import Client
 
 # pytest는 기본적으로 DB접근을 막고 있어요.
 
 @pytest.mark.django_db
-def test_blog_index():
+def test_blog_index(client):  # pytest에서 fixture
     # 아래 테스트는 View 단에서 DB접근이 발생합니다.
-    client = Client()
     res = client.get("/blog/")
     assert res.status_code == 200
