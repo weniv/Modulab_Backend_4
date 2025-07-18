553e0551d7e7e3489d4178ae2aa915701e4e1c46
Chinseok Lee <me@pyhub.kr>
2025-07-18 10:36:52 +0900

    ModelViewSet을 통해서 Post 5개 API를 구현했습니다.

--- pyhub/mydjango/blog/api/v1.py
+++ pyhub/mydjango/blog/api/v1.py
@@ -1,43 +1,19 @@
 # blog/api/v1.py
 
-from django.urls import path
-from rest_framework.generics import ListAPIView, CreateAPIView, RetrieveAPIView
-from rest_framework.permissions import IsAuthenticated
-
-from blog.api.serializers import PostSerializer, PostListSerializer, PostCreateSerializer
-
-
-post_list = ListAPIView.as_view(
-    queryset=PostListSerializer.get_optimized_queryset(),
-    serializer_class=PostListSerializer,  # 고정. but 동적 지정도 가능.
-)
+from rest_framework.routers import DefaultRouter
+from rest_framework.viewsets import ModelViewSet
+from blog.api.serializers import PostSerializer
+from blog.models import Post
 
 
-# 아래 모델 시리얼라이저에 지정된 필드 만으로 데이터베이스 저장이 가능할 때에는
-# 아래 코드 만으로 충분합니다.
-# 그런데, 데이터베이스 저장 전에 추가로 할당해야할 필드 (ex: author)가 있다면
-# 데이터베이스 저장 전에 할당을 해줘야겠죠.
-
-class PostCreateAPIView(CreateAPIView):
-    serializer_class = PostCreateSerializer
-    permission_classes = [IsAuthenticated]  # @login_required 장식자와 비슷
-
-    # 웹페이지에서 로그인된 상황에서 테스트해봅시다.
-    def perform_create(self, serializer):
-        serializer.save(author=self.request.user)
+class PostViewSet(ModelViewSet):
+    queryset = Post.objects.all()  # 고정. 가변적으로 줄려면 get_queryset 메서드를 구현.
+    serializer_class = PostSerializer  # 고정. 가변적으로 줄려면 get_serializer_class 메서드를 구현.
 
 
-post_new = PostCreateAPIView.as_view()
+router = DefaultRouter()
+router.register("posts", PostViewSet)
+# router.urls  # router가 자동 생성해낸 urlpatterns 리스트
 
 
-post_detail = RetrieveAPIView.as_view(
-    queryset=PostSerializer.get_optimized_queryset(),
-    serializer_class=PostSerializer,
-)
-
-
-urlpatterns = [
-    # path("posts/", post_list),
-    path("posts/", post_new),
-    path("posts/<int:pk>/", post_detail),
-]
+urlpatterns = router.urls
